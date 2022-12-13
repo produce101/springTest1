293a4b4e3d269c56b7b83db151924265151e967b
ajm <hawaiicola@digitalmarket.kr>
2022-12-13 10:36:56 +0900

    임포트 주석

--- src/main/java/auth/SampleInterceptor.java
+++ src/main/java/auth/SampleInterceptor.java
@@ -1,12 +1,10 @@
 package auth;
 
-import org.eclipse.osgi.internal.loader.SystemBundleLoader;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
